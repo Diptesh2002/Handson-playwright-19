c4562be7e2d3ede7d0e2016e9f4ba6f75d044c04
Diptesh Panda <90669997+Diptesh2002@users.noreply.github.com>
2025-07-20 20:49:24 +0530

    Third Commit to fail the test

--- tests/handson19.spec.js
+++ tests/handson19.spec.js
@@ -4,7 +4,7 @@ test('Handson19',async({page})=>{
     await page.locator("//a[@class='btn btn-lg'][normalize-space()='Complete Web Form']").click();
     await page.getByPlaceholder('Enter first name').fill('Diptesh');
     await page.getByPlaceholder('Enter last name').fill('Panda');
-    await page.getByPlaceholder('Enter your job title').fill('Automation Testing');
+    await page.getByPlaceholder('Enter your job title').fil('Automation Testing');
     await expect(page.locator("//input[@id='radio-button-3']")).not.toBeChecked();
     await page.locator("//input[@id='radio-button-3']").check();
     await page.locator("//input[@id='checkbox-1']").click();
